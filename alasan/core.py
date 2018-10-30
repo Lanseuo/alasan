1533727eae8575253398f08021f0bd22534237e4
Lucas Hild <contact@lucas-hild.de>
2018-10-30 16:31:28 +0100

    Add on_session_ended

--- alasan/core.py
+++ alasan/core.py
@@ -4,11 +4,13 @@ from .errors import IntentNotFound
 
 class Alasan:
     def __init__(self):
-        self.launch_function = None
+        self.launch_function = lambda x: {}
         self.intent_functions = {}
+        self.on_session_ended_function = lambda x: {}
 
     def __call__(self, event_dict, context):
         print("IN:", event_dict)
+
         event = Event(event_dict)
 
         if event.request.type == "LaunchRequest":
@@ -19,6 +21,9 @@ class Alasan:
                 raise IntentNotFound
 
             response = self.intent_functions[event.request.intent.name](event)
+
+        elif event.request.type == "SessionEndedRequest":
+            response = self.on_session_ended_function(event)
 
         print("OUT:", response)
         return response
@@ -33,6 +38,13 @@ class Alasan:
     def intent(self, intent_name):
         def wrap(f):
             self.intent_functions[intent_name] = f
+            return f
+
+        return wrap
+
+    def on_session_ended(self):
+        def wrap(f):
+            self.on_session_ended_function = f
             return f
 
         return wrap
