dc042f0d48122b9e0c3826c798c094afaaffb032
Lucas Hild <contact@lucas-hild.de>
2018-12-28 18:47:32 +0100

    Add cards to response

--- alasan/__init__.py
+++ alasan/__init__.py
@@ -1,4 +1,10 @@
-from .core import Alasan
-from .response import Response
+# Card Type (Response)
+SIMPLE = "Simple"
+STANDARD = "Standard"
+LINK_ACCOUNT = "LinkAccount"
+ASK_FOR_PERMISSIONS_CONSENT = "AskForPermissionsConsent"
+
+from .core import Alasan  # noqa
+from .response import Response  # noqa
 
 __all__ = ["Alasan", "Response"]
--- alasan/response.py
+++ alasan/response.py
@@ -1,3 +1,6 @@
+from . import SIMPLE, STANDARD, LINK_ACCOUNT, ASK_FOR_PERMISSIONS_CONSENT
+
+
 class Response:
     def __init__(self):
         self.data_dict = {
@@ -25,8 +28,27 @@ class Response:
             }
         return self
 
-    def card(self, card_dict):
-        self.data_dict["response"]["card"] = card_dict
+    def card(self, type_, title=None, content=None, text=None, image=None):
+        self.data_dict["response"]["card"] = {}
+
+        if type_ not in [SIMPLE, STANDARD, LINK_ACCOUNT, ASK_FOR_PERMISSIONS_CONSENT]:
+            raise ValueError(
+                "Card type has to be one of the following: SIMPLE, STANDARD, LINK_ACCOUNT, ASK_FOR_PERMISSIONS_CONSENT")
+
+        self.data_dict["response"]["card"]["type"] = type_
+
+        if title:
+            self.data_dict["response"]["card"]["title"] = title
+        if content:
+            self.data_dict["response"]["card"]["content"] = content
+        if text:
+            self.data_dict["response"]["card"]["text"] = text
+        if image:
+            self.data_dict["response"]["card"]["image"] = {
+                "smallImageUrl": image["small"],
+                "largeImageUrl": image["large"]
+            }
+
         return self
 
     def reprompt(self, text, ssml=False):
