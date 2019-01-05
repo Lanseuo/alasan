198a340b74429ccab2f4095c39b245936c60ea4c
Lucas Hild <contact@lucas-hild.de>
2019-01-05 21:49:23 +0100

    Add build command to cli

--- alasan/cli.py
+++ alasan/cli.py
@@ -1,5 +1,9 @@
 import click
-import os
+import shutil
+import subprocess
+import sys
+
+from pathlib import Path
 
 
 @click.group()
@@ -11,10 +15,37 @@ def cli():
 @click.argument("project_name")
 def create(project_name):
     """Create a new skill using the default template."""
-    return
+    pass
 
 
 @cli.command()
 def deploy():
     """Deploy a skill to AWS Lambda."""
-    return
+    pass
+
+
+@cli.command()
+def build():
+    """Build your skill to skill.zip including dependencies"""
+    install_dependency("git+https://github.com/Lanseuo/alasan.git")
+    install_requirements()
+    shutil.copy("skill.py", ".alasan/build")
+
+    shutil.make_archive("skill", 'zip', ".alasan/build")
+    shutil.rmtree(".alasan/build")
+    print("Created skill.zip")
+
+
+def install_requirements():
+    requirements_file = Path("requirements.txt")
+    if not requirements_file.exists():
+        return
+
+    install_dependency(["-r", requirements_file])
+
+
+def install_dependency(package):
+    if isinstance(package, list):
+        subprocess.check_call([sys.executable, "-m", "pip", "install", *package, "-t", ".alasan/build"])
+    else:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package, "-t", ".alasan/build"])
